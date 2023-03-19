649582986dc6a35d2f04400109b1e4934a59faa6
Astroneural <Astroneural@users.noreply.github.com>
2023-03-19 21:02:39 +0000

    deploy: bb5e993b257a40b7c19d2aeec8b87adaf6921986

---
+++ _app/immutable/chunks/3-98b67170.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about-me/_page.svelte.md-eb221b7e.js";export{t as component};
---
+++ _app/immutable/chunks/5-76e9a0ff.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/keynote/chess-popular/_page.svelte.md-1c668533.js";export{t as component};
