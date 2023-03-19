ae62ad8298a7f2f2730972d6377e094a8ffbfe9a
Shaum <105980825+Astroneural@users.noreply.github.com>
2023-03-18 19:12:15 -0500

    Update icon.ts

--- src/lib/config/icon.ts
+++ src/lib/config/icon.ts
@@ -2,7 +2,7 @@ import type { Icon } from '$lib/types/icon'
 import { site } from '$lib/config/site'
 
 export const favicon: Icon = {
-  src: site.protocol + site.domain + '/favicon.png',
+  src: 'https://shau.ml/favicon_48x42.png', // restructure like others later
   sizes: '48x42',
   type: 'image/png'
 }
