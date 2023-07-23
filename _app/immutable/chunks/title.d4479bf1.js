c60a4cb62dc6f612a54b676089dd708085c14b85
Astroneural <Astroneural@users.noreply.github.com>
2023-07-23 02:05:04 +0000

    deploy: ee533e81cfcd47577c61da9224730b68b9cfdc55

--- _app/immutable/chunks/title.d4479bf1.js
+++
@@ -1,3 +0,0 @@
-import{a0 as r}from"./index.e01e33ca.js";import{w as n}from"./index.77016c14.js";function _(s){const t=s-1;return t*t*t+1}function U(s,{delay:t=0,duration:e=400,easing:i=_,x:f=0,y:p=0,opacity:u=0}={}){const o=getComputedStyle(s),a=+o.opacity,l=o.transform==="none"?"":o.transform,m=a*(1-u),[y,$]=r(f),[b,g]=r(p);return{delay:t,duration:e,easing:i,css:(c,x)=>`
-			transform: ${l} translate(${(1-c)*y}${$}, ${(1-c)*b}${g});
-			opacity: ${a-m*x}`}}const V={},C=n([]),O=n([]),S=n({});export{C as a,O as b,U as f,V as p,S as t};
